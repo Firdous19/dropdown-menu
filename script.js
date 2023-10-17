834802e3451900a9b6543a25f04935c3a11bd239
Firdous <firdousrahaman42@gmail.com>
2023-10-17 17:13:43 +0530

    Remove commented code and added some extra code

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const mobilecompanyButton = document.querySelector('.mobile-navbar-left').queryS
 const downArrow = document.querySelectorAll('.downarrow');
 const mobileDownArrow = document.querySelectorAll('.mobile-downarrow');
 
-
+let navCutStatus = false;
 
 
 featuresButton.addEventListener('click', () => {
@@ -26,6 +26,20 @@ companyButton.addEventListener('click', () => {
 });
 
 hamburgermenuButton.addEventListener('click', () => {
+    if (!navCutStatus) {
+        hamburgermenuButton.innerHTML = `
+        <button id="hamburger-menu-button2"><svg width="26" height="26" xmlns="http://www.w3.org/2000/svg"><g fill="#151515" fill-rule="evenodd"><path d="m2.393.98 22.628 22.628-1.414 1.414L.979 2.395z"/><path d="M.98 23.607 23.609.979l1.414 1.414L2.395 25.021z"/></g></svg></button>
+        `;
+        navCutStatus = true;
+    }
+    else {
+        hamburgermenuButton.innerHTML = `<button id="hamburger-menu-button2"><svg width="32" height="18" xmlns="http://www.w3.org/2000/svg">
+          <g fill="#151515" fill-rule="evenodd">
+            <path d="M0 0h32v2H0zM0 8h32v2H0zM0 16h32v2H0z" />
+          </g>
+        </svg></button>`
+        navCutStatus = false;
+    }
     hamburgerMenu.classList.toggle('active');
     backDrop.classList.toggle('visible');
 });
